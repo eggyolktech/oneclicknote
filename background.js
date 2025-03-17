c979a1eb42e5a45acddaf5da1d49527bfbaa8d06
eggyolktech <eggyolktech@gmail.com>
2025-03-17 14:10:31 +0800

    Try to redirect to new note tab directly

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'openGoogleKeep') {
-    chrome.tabs.create({ url: 'https://keep.google.com/' }, (tab) => {
+    chrome.tabs.create({ url: 'https://keep.google.com/u/0/' }, (tab) => {
       if (chrome.runtime.lastError) {
         console.error('Error creating tab:', chrome.runtime.lastError);
       }
